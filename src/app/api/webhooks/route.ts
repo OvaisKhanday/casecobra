659773d13fd07947426483e44f2b65ee68d48535
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-05-17 12:36:26 +0530

    change to-email

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -57,7 +57,7 @@ export async function POST(req: NextRequest) {
       });
 
       await resend.emails.send({
-        from: "casecobra <ovaiskhanday927@gmail.com>",
+        from: "casecobra <onboarding@resend.dev>",
         to: [event.data.object.customer_details.email],
         subject: "Thanks for your order!",
         react: OrderReceivedEmail({
